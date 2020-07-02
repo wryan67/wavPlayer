04740bf780d098515f22964226dc8f91cc0ca5cd
wade ryan <wryan67@gmail.com>
2020-07-02 12:48:13 -0500

    turn off interrupts

--- wavPlayer/main.cpp
+++ wavPlayer/main.cpp
@@ -10,6 +10,11 @@
 #include <pthread.h>
 #include <lcd.h>
 #include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
 
 #include <chrono>
 #include <ctime>  
@@ -43,6 +48,129 @@ struct wavHeader
 };
 
 
+volatile unsigned* gpio, * gpset, * gpclr, * gpin, * timer, * intrupt;
+#define GPIO_BASE  0x20200000
+#define TIMER_BASE 0x20003000
+#define INT_BASE 0x2000B000
+
+/***************** SETUP ****************
+Sets up five GPIO pins as described in comments
+Sets timer and interrupt pointers for future use
+Does not disable interrupts
+return 1 = OK
+       0 = error with message print
+************************************/
+
+int setup2() {
+    int memfd;
+    unsigned int timend;
+    void* gpio_map, * timer_map, * int_map;
+
+    memfd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (memfd < 0) {
+        printf("Mem open error\n");
+        return(0);
+    }
+
+    gpio_map = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+        MAP_SHARED, memfd, GPIO_BASE);
+
+    timer_map = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+        MAP_SHARED, memfd, TIMER_BASE);
+
+    int_map = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+        MAP_SHARED, memfd, INT_BASE);
+
+    close(memfd);
+
+    if (gpio_map == MAP_FAILED ||
+        timer_map == MAP_FAILED ||
+        int_map == MAP_FAILED) {
+        printf("Map failed\n");
+        return(0);
+    }
+    // interrupt pointer
+    intrupt = (volatile unsigned*)int_map;
+    // timer pointer
+    timer = (volatile unsigned*)timer_map;
+    ++timer;    // timer lo 4 bytes
+                // timer hi 4 bytes available via *(timer+1)
+
+                // GPIO pointers
+    gpio = (volatile unsigned*)gpio_map;
+    gpset = gpio + 7;     // set bit register offset 28
+    gpclr = gpio + 10;    // clr bit register
+    gpin = gpio + 13;     // read all bits register
+
+        // setup  GPIO 2/3 = inputs    have pull ups on board
+        //        control reg = gpio + 0 = pin/10
+        //        GPIO 2 shift 3 bits by 6 = (pin rem 10) * 3
+        //        GPIO 3 shift 3 bits by 9 = (pin rem 10) * 3
+
+
+    return(1);
+}
+
+/******************** INTERRUPTS *************
+
+Is this safe?
+Dunno, but it works
+
+interrupts(0)   disable interrupts
+interrupts(1)   re-enable interrupts
+
+return 1 = OK
+       0 = error with message print
+
+Uses intrupt pointer set by setup()
+Does not disable FIQ which seems to
+cause a system crash
+Avoid calling immediately after keyboard input
+or key strokes will not be dealt with properly
+
+*******************************************/
+
+int interrupts(int flag) {
+    static unsigned int sav132 = 0;
+    static unsigned int sav133 = 0;
+    static unsigned int sav134 = 0;
+
+    if (flag == 0)    // disable
+    {
+        if (sav132 != 0) {
+            // Interrupts already disabled so avoid printf
+            return(0);
+        }
+
+        if ((*(intrupt + 128) | *(intrupt + 129) | *(intrupt + 130)) != 0) {
+            printf("Pending interrupts\n");  // may be OK but probably
+            return(0);                       // better to wait for the
+        }                                // pending interrupts to
+                                         // clear
+
+        sav134 = *(intrupt + 134);
+        *(intrupt + 137) = sav134;
+        sav132 = *(intrupt + 132);  // save current interrupts
+        *(intrupt + 135) = sav132;  // disable active interrupts
+        sav133 = *(intrupt + 133);
+        *(intrupt + 136) = sav133;
+    }
+    else            // flag = 1 enable
+    {
+        if (sav132 == 0) {
+            printf("Interrupts not disabled\n");
+            return(0);
+        }
+
+        *(intrupt + 132) = sav132;    // restore saved interrupts
+        *(intrupt + 133) = sav133;
+        *(intrupt + 134) = sav134;
+        sav132 = 0;                 // indicates interrupts enabled
+    }
+    return(1);
+}
+
+
 
 bool setup() {
 
@@ -50,6 +178,13 @@ bool setup() {
 		fprintf(stderr, "Wiring Pi setup failed, ret=%d\n", ret);
 		return false;
 	}
+
+
+    if (piHiPri(1)) {
+        printf("high priority failed\n");
+        return 2;
+    }
+
 
 
 	int seed;
@@ -128,6 +263,7 @@ void playFile(char *filename) {
 		return;
 	}
 	printf("Playing %s...\n", filename);
+
 
 	wavHeader wavHeader;
 	
@@ -215,6 +351,10 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+    setup2();
+    printf("waiting for interrupts to clear...\n");
+    delay(2000);
+    interrupts(0);
 
 	for (int i = 0; i < wavFiles; ++i) {
 		playFile(wavFileNames[i]);
@@ -222,6 +362,9 @@ int main(int argc, char **argv)
 
 	fflush(stdout);
 	//		delay(4294967295U);   // 3.27 years
+
+    interrupts(1);
+
 	return 0;
 
 }
